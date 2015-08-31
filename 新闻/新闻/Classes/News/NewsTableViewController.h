943fb7944cd7646885a8dc5cd2874a06b7c16ee6
AlexFeng <411587131@qq.com>
2015-08-31 23:35:08 +0800

    搭建项目框架

--- 新闻/新闻/Classes/News/NewsTableViewController.h
+++ 新闻/新闻/Classes/News/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  新闻
 //
 //  Created by AlexFeng on 15/8/31.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
